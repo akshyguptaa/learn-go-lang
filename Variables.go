b587c43b7a05d7bbca64fe7c1ed7bd5238da7ed5
Akshay Gupta <akshay_gupta15@yahoo.com>
2025-11-30 00:19:36 +0530

    Add Variables.go

---
+++ Variables.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	//This following assignment is called "type interface"
+	//Only works inside functions
+	helloWorld := "Hello, World!"
+	fmt.Println(helloWorld)
+
+	//Assign a different value to the variable
+	helloWorld = "World, Hello!"
+	fmt.Println(helloWorld)
+
+
+	//Variable declarations:
+	//var variable type
+ 	//var variable type = value
+ 	//var variable = value
+ 	//var variable1, variable2 type = value1, value
+
+	var name string
+	var age int = 24
+	var weight = 62.65
+	var width, height int = 3,4
+
+
+	//variable declaration using var keyword can be at package level or inside function.
+	//Variables declared at the function level must be used. Otherwise,the compiler is going to throw an error during compilation.
+}
